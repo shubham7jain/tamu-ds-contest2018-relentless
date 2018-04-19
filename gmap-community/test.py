487f8dce6861aa9897317987cfc269f030a72e55
shubham7jain <shubham7jain@gmail.com>
2018-04-19 18:37:55 -0500

    Removed

--- gmap-community/test.py
+++
@@ -1,8 +0,0 @@
-import json
-
-with open('test.json') as f:
-    data = json.load(f)
-
-for feature in data['features']:
-    print(feature['geometry']['type'])
-    print(feature['geometry']['coordinates'])
